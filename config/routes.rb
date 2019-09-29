32844a6d1bd4fa5d4c3cf17b2addf825d54a601e
JoelDeTrinidad <joeldetrinidad@gmail.com>
2019-09-29 17:19:27 -0600

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
-  root 'pages#index'
+  root 'articles#index'
   get 'pages/contact'
   resources :tasks
   resources :articles
